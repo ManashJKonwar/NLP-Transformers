82288e4775b661850fc91eb61767de16a70442ac
ManashJKonwar <rickykonwar@gmail.com>
2022-08-17 23:34:13 +0530

    Update: Minor_fix_to_dataset_loader_script
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- bert/ner/dataset.py
+++ bert/ner/dataset.py
@@ -35,32 +35,32 @@ class EntityDataset:
             target_pos.extend([pos[i]] * input_len)
             target_tag.extend([tags[i]] * input_len)
 
-            # Padding the text
-            ids = ids[:config.MAX_LEN - 2] # -2 is done for special tokens
-            target_pos = target_pos[:config.MAX_LEN - 2]
-            target_tag = target_tag[:config.MAX_LEN - 2]
+        # Padding the text
+        ids = ids[:config.MAX_LEN - 2] # -2 is done for special tokens
+        target_pos = target_pos[:config.MAX_LEN - 2]
+        target_tag = target_tag[:config.MAX_LEN - 2]
 
-            ids = [101] + ids + [102] # 101 and 102 represents [CLS] and [SEP] token
-            target_pos = [0] + target_pos + [0] # adding 0 to replicate CLS and SEP addition in pos
-            target_tag = [0] + target_tag + [0] # adding 0 to replicate CLS and SEP addition in tags
+        ids = [101] + ids + [102] # 101 and 102 represents [CLS] and [SEP] token
+        target_pos = [0] + target_pos + [0] # adding 0 to replicate CLS and SEP addition in pos
+        target_tag = [0] + target_tag + [0] # adding 0 to replicate CLS and SEP addition in tags
 
-            # Attention mask
-            mask = [1] * len(ids)
-            token_type_ids = [0] * len(ids)
+        # Attention mask
+        mask = [1] * len(ids)
+        token_type_ids = [0] * len(ids)
 
-            # Padding input
-            padding_len = config.MAX_LEN - len(ids)
+        # Padding input
+        padding_len = config.MAX_LEN - len(ids)
 
-            ids = ids + ([0] * padding_len)
-            mask = mask + ([0] * padding_len)
-            token_type_ids = token_type_ids + ([0] * padding_len)
-            target_pos = target_pos + ([0] * padding_len)
-            target_tag = target_tag + ([0] * padding_len)
+        ids = ids + ([0] * padding_len)
+        mask = mask + ([0] * padding_len)
+        token_type_ids = token_type_ids + ([0] * padding_len)
+        target_pos = target_pos + ([0] * padding_len)
+        target_tag = target_tag + ([0] * padding_len)
 
-            return {
-                "ids": torch.tensor(ids, dtype=torch.long),
-                "mask": torch.tensor(mask, dtype=torch.long),
-                "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
-                "target_pos": torch.tensor(target_pos, dtype=torch.long),
-                "target_tag": torch.tensor(target_tag, dtype=torch.long),
-            }
+        return {
+            "ids": torch.tensor(ids, dtype=torch.long),
+            "mask": torch.tensor(mask, dtype=torch.long),
+            "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
+            "target_pos": torch.tensor(target_pos, dtype=torch.long),
+            "target_tag": torch.tensor(target_tag, dtype=torch.long),
+        }
