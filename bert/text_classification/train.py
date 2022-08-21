f1ec25a35745df92ac805b9a31f7a2f34126f72a
ManashJKonwar <rickykonwar@gmail.com>
2022-08-21 22:12:59 +0530

    Enhancement: Adding_training_script_for_custom_text_classifier
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

---
+++ bert/text_classification/train.py
@@ -0,0 +1,108 @@
+__author__ = "konwar.m"
+__copyright__ = "Copyright 2022, AI R&D"
+__credits__ = ["konwar.m"]
+__license__ = "Individual Ownership"
+__version__ = "1.0.1"
+__maintainer__ = "konwar.m"
+__email__ = "rickykonwar@gmail.com"
+__status__ = "Development"
+
+import pandas as pd
+import numpy as np
+
+import joblib
+import torch
+
+from sklearn import preprocessing
+from sklearn import model_selection
+
+from transformers import AdamW
+from transformers import get_linear_schedule_with_warmup
+
+import config
+import dataset
+import engine
+from model import TextClassifierModel
+
+def process_data(data_path):
+    df = pd.read_csv(data_path)
+
+    enc_labels = preprocessing.LabelEncoder()
+    df.loc[:, 'category'] = enc_labels.fit_transform(df['category'])
+
+    sentences = df.text.tolist()
+    labels = df.category.tolist()
+
+    return sentences, labels, enc_labels
+
+if __name__ == "__main__":
+    sentences, labels, enc_labels = process_data(config.TRAINING_FILE)
+
+    meta_data = {
+        "enc_labels": enc_labels
+    }
+
+    joblib.dump(meta_data, "output/text_classification/meta.bin")
+
+    num_labels = len(list(enc_labels.classes_))
+
+    (
+        train_sentences,
+        test_sentences,
+        train_labels,
+        test_labels
+    ) = model_selection.train_test_split(sentences, labels, random_state=42, test_size=0.1)
+
+    train_dataset = dataset.TextClassifierDataset(
+        texts=train_sentences, labels=train_labels
+    )
+
+    train_data_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=config.TRAIN_BATCH_SIZE, num_workers=4
+    )
+
+    valid_dataset = dataset.TextClassifierDataset(
+        texts=test_sentences, labels=test_labels
+    )
+
+    valid_data_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=config.VALID_BATCH_SIZE, num_workers=1
+    )
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    model = TextClassifierModel(num_labels=num_labels)
+    model.to(device)
+
+    param_optimizer = list(model.named_parameters())
+    no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
+    optimizer_parameters = [
+        {
+            "params": [
+                p for n, p in param_optimizer if not any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.001,
+        },
+        {
+            "params": [
+                p for n, p in param_optimizer if any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.0,
+        },
+    ]
+
+    num_train_steps = int(len(train_sentences) / config.TRAIN_BATCH_SIZE * config.EPOCHS)
+    optimizer = AdamW(optimizer_parameters, lr=config.LEARNING_RATE)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=0, num_training_steps=num_train_steps
+    )
+
+    best_loss = np.inf
+    for epoch in range(config.EPOCHS):
+        train_acc, train_loss = engine.train_fn(train_data_loader, model, optimizer, device, scheduler)
+        test_acc, test_loss = engine.eval_fn(valid_data_loader, model, device)
+        print(f"Train Loss = {train_loss} Valid Loss = {test_loss}")
+        if test_loss < best_loss:
+            torch.save(model.state_dict(), config.MODEL_PATH)
+            best_loss = test_loss
