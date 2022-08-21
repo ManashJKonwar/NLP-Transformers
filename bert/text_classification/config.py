ce3ce0a35aca93ad9901bda2acb2ec9d1a7fcc59
ManashJKonwar <rickykonwar@gmail.com>
2022-08-21 22:13:24 +0530

    Enhancement: Adding_config_script_for_custom_text_classifier
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

---
+++ bert/text_classification/config.py
@@ -0,0 +1,22 @@
+__author__ = "konwar.m"
+__copyright__ = "Copyright 2022, AI R&D"
+__credits__ = ["konwar.m"]
+__license__ = "Individual Ownership"
+__version__ = "1.0.1"
+__maintainer__ = "konwar.m"
+__email__ = "rickykonwar@gmail.com"
+__status__ = "Development"
+
+import transformers
+
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 2
+VALID_BATCH_SIZE = 2
+EPOCHS = 5
+LEARNING_RATE = 1e-6
+BASE_MODEL_PATH = "pretrained_models/bert/bert_base_cased"
+MODEL_PATH = "output/text_classification/model.bin"
+TRAINING_FILE = "input/text_classification/text_classification_dataset.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BASE_MODEL_PATH
+)
