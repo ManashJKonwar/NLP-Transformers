0673145ba132e1b5be75d82dfb90a3b45853d51a
ManashJKonwar <rickykonwar@gmail.com>
2022-08-24 21:33:01 +0530

    Enhancement: Adding_config_script_for_custom_sentiment_analyzer
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

---
+++ bert/sentiment_analysis/config.py
@@ -0,0 +1,22 @@
+__author__ = "konwar.m"
+__copyright__ = "Copyright 2022, AI R&D"
+__credits__ = ["konwar.m"]
+__license__ = "Individual Ownership"
+__version__ = "1.0.1"
+__maintainer__ = "konwar.m"
+__email__ = "rickykonwar@gmail.com"
+__status__ = "Development"
+
+import transformers
+
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 8
+VALID_BATCH_SIZE = 4
+EPOCHS = 10
+BASE_MODEL_PATH = "pretrained_models/bert/bert_base_uncased"
+MODEL_PATH = "output/sentiment_analysis/model.bin"
+TRAINING_FILE = "input/sentiment_analysis/sentiment_analysis_dataset.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BASE_MODEL_PATH, 
+    do_lower_case=True
+)
