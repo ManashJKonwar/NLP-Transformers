cc895dca63ae91a56a11d72c0d7f209893b8757c
ManashJKonwar <rickykonwar@gmail.com>
2022-08-30 22:20:55 +0530

    Enhancement: Adding_inferencing_script_for_custom_sentiment_analyzer
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- bert/sentiment_analysis/predict.py
+++ bert/sentiment_analysis/predict.py
@@ -17,7 +17,7 @@ from model import SentimentAnalyzerModel
 if __name__ == "__main__":
 
     sentence = """
-
+    How this film could be classified as Drama, I have no idea. If I were John Voight and Mary Steenburgen, I would be trying to erase this from my CV. It was as historically accurate as Xena and Hercules. Abraham and Moses got melded into Noah. Lot, Abraham's nephew, Lot, turns up thousands of years before he would have been born. Canaanites wandered the earth...really? What were the scriptwriters thinking? Was it just ignorance (""I remember something about Noah and animals, and Lot and Canaanites and all that stuff from Sunday School"") or were they trying to offend the maximum number of people on the planet as possible- from Christians, Jews and Muslims, to historians, archaeologists, geologists, psychologists, linguists ...as a matter of fact, did anyone not get offended? Anyone who had even a modicum of taste would have winced at this one!
     """
 
     review = str(sentence)
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         targets=[0]
     )
 
-    test_data_loader = torch.utils.data.Dataloader(
+    test_data_loader = torch.utils.data.DataLoader(
         test_dataset, batch_size=1, num_workers=1
     )
 
@@ -47,3 +47,16 @@ if __name__ == "__main__":
     else:
         model.load_state_dict(torch.load(config.MODEL_PATH, map_location=torch.device('cpu')))
     model.to(device)
+
+    with torch.no_grad():
+        for test_ip in test_data_loader:
+            mask = test_ip['mask'].to(device)
+            input_id = test_ip['ids'].to(device)
+            token_type_ids = test_ip['token_type_ids'].to(device)
+
+            output = model(input_id, mask, token_type_ids)
+            pos_prediction = torch.sigmoid(output).cpu().detach().numpy()[0][0]
+            if pos_prediction<0.5:
+                print('Negative')
+            else:
+                print('Positive')
