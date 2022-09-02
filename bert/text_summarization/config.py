191a54d1d42328727a979e92d71a4118bd4ea7ec
ManashJKonwar <rickykonwar@gmail.com>
2022-09-02 23:30:17 +0530

    Enhancement: Initial_draft_of_config_for_text_summarizer
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- bert/text_summarization/config.py
+++ bert/text_summarization/config.py
@@ -6,3 +6,17 @@ __version__ = "1.0.1"
 __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
+
+import transformers
+
+MAX_LEN = 128
+TRAIN_BATCH_SIZE = 32
+VALID_BATCH_SIZE = 8
+EPOCHS = 10
+BASE_MODEL_PATH = "pretrained_models/bert/bert_base_uncased"
+MODEL_PATH = "output/text_summarization/model.bin"
+TRAINING_FILE = "input/text_summarization/text_summarization_dataset.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BASE_MODEL_PATH,
+    do_lower_case=True
+)
