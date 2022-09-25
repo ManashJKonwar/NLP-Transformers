0e101307fd14a468b944a241a09aa635934916bd
ManashJKonwar <rickykonwar@gmail.com>
2022-09-25 23:59:45 +0530

    Enhancement: Setting_up_dataset_script_for_extractive_text_summarizer
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- bert/text_summarization/dataset.py
+++ bert/text_summarization/dataset.py
@@ -6,3 +6,41 @@ __version__ = "1.0.1"
 __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
+
+import config
+
+class TextSummarizerDataset:
+    def __init__(self, data):
+        self._data = data
+
+    def __len__(self):
+        return len(self._data)
+
+    def __getitem__(self, item):
+        # Extracts specific articles and there summary
+        sentence = str(self._data.iloc[item].sents)
+        sentence = " ".join(sentence.split())
+        
+        article = str(self._data.iloc[item].docs)
+        article = " ".join(article.split())
+
+        inputs = config.TOKENIZER.batch_encode_plus(
+            [sentence, article],
+            add_special_tokens=True,
+            max_length=config.MAX_LEN,
+            padding="max_length",
+            return_token_type_ids=True,
+            truncation=True
+        )
+
+        # Extracting BERT ids, attention mask and token ids
+        ids = inputs["input_ids"]
+        mask = inputs["attention_mask"]
+
+        return {
+            "sent_ids": torch.tensor(ids[0], dtype=torch.long),
+            "doc_ids": torch.tensor(ids[1], dtype=torch.long),
+            "sent_mask": torch.tensor(mask[0], dtype=torch.long),
+            "doc_mask": torch.tensor(mask[1], dtype=torch.long),
+            "targets": torch.tensor([self._data.iloc[item].y], dtype=torch.long)
+        } 
