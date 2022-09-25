bcd1b3f7337e57a04a9778532930fb9737a0f917
ManashJKonwar <rickykonwar@gmail.com>
2022-09-26 00:02:17 +0530

    Enhancement: Adding_model_script_for_extractive_text_summarizer
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- bert/text_summarization/model.py
+++ bert/text_summarization/model.py
@@ -6,3 +6,45 @@ __version__ = "1.0.1"
 __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
+
+import config
+import torch
+import transformers
+import torch.nn as nn
+
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+    return sum_embeddings / sum_mask
+
+class TextSummarizerModel(nn.Module):
+    def __init__(self, n_feature=512, dropout=0.3):
+        super(TextSummarizerModel, self).__init__()
+        self.bert =transformers.BertModel.from_pretrained(config.BASE_MODEL_PATH) 
+        self.pre_classifier = nn.Linear(n_feature*3, 768)
+        self.dropout = nn.Dropout(dropout)
+        self.classifier = nn.Linear(768, 1)
+        self.classifierSigmoid = nn.Sigmoid()
+
+    def forward(self, sent_ids, doc_ids, sent_mask, doc_mask):
+        sent_output = self.bert(input_ids=sent_ids, attention_mask=sent_mask) 
+        sentence_embeddings = mean_pooling(sent_output, sent_mask) 
+        
+        article_output = self.bert(input_ids=doc_ids, attention_mask=doc_mask) 
+        article_embeddings = mean_pooling(doc_output, doc_mask)
+
+        # Elementwise product of sentence embeddings and article embeddings
+        combined_features = sentence_embeddings * doc_embeddings  
+
+        # Concatenate input features and their elementwise product
+        concat_features = torch.cat((sentence_embeddings, doc_embeddings, combined_features), dim=1)   
+        
+        pooler = self.pre_classifier(concat_features) 
+        pooler = nn.ReLU()(pooler)
+        pooler = self.dropout(pooler)
+        output = self.classifier(pooler)
+        final_layer = self.classifierSigmoid(output) 
+
+        return final_layer
