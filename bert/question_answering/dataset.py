2a471a5666629616fa1049c129163c61787f5048
ManashJKonwar <rickykonwar@gmail.com>
2022-09-29 23:00:48 +0530

    Enhancement: Adding_placeholder_scripts_for_question_answering_task
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

---
+++ bert/question_answering/dataset.py
@@ -0,0 +1,8 @@
+__author__ = "konwar.m"
+__copyright__ = "Copyright 2022, AI R&D"
+__credits__ = ["konwar.m"]
+__license__ = "Individual Ownership"
+__version__ = "1.0.1"
+__maintainer__ = "konwar.m"
+__email__ = "rickykonwar@gmail.com"
+__status__ = "Development"
