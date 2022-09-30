9cc1e202c70440eb46551b416912836f6612ae7a
ManashJKonwar <rickykonwar@gmail.com>
2022-09-30 23:01:41 +0530

    Enhancement: Adding_config_for_question_answering_task
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- bert/question_answering/config.py
+++ bert/question_answering/config.py
@@ -6,3 +6,19 @@ __version__ = "1.0.1"
 __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
+
+import transformers
+
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 4
+VALID_BATCH_SIZE = 4
+EPOCHS = 10
+BASE_MODEL_PATH = "pretrained_models/bert/bert_base_uncased"
+MODEL_PATH = "output/question_answering/model.bin"
+QnA_TRAINING_PATH = "input/question_answering/train-v1.1.json"
+QnA_VALIDATION_PATH = "input/question_answering/dev-v1.1.json"
+TRAINING_FILE = "input/question_answering/question_answering_dataset.csv"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BASE_MODEL_PATH,
+    do_lower_case=True
+)
