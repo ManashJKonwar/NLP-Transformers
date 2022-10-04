6d5d3675fdfb876e22b8f76946f8e3f8efbd493c
ManashJKonwar <rickykonwar@gmail.com>
2022-10-04 23:19:20 +0530

    Enhancement: Adding_model_script_for_question_answering_task
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- bert/question_answering/model.py
+++ bert/question_answering/model.py
@@ -6,3 +6,15 @@ __version__ = "1.0.1"
 __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
+
+import config
+import transformers
+import torch.nn as nn
+
+class QuestionAnsweringModel(nn.Module):
+    def __init__(self):
+        super(QuestionAnsweringModel, self).__init__()
+        self.bert = transformers.BertModel.from_pretrained(config.BASE_MODEL_PATH)
+
+    def forward(self):
+        pass
