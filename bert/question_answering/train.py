76661f2bf5851aedff2a9b9b8755508c0038e4c8
ManashJKonwar <rickykonwar@gmail.com>
2022-10-04 23:18:38 +0530

    Enhancement: Adding_training_script_for_question_answering_task
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- bert/question_answering/train.py
+++ bert/question_answering/train.py
@@ -13,11 +13,18 @@ import json
 import numpy as np
 import pandas as pd
 
+import torch
+
 from tqdm import tqdm
 from sklearn import model_selection
 
+from transformers import AdamW
+from transformers import get_linear_schedule_with_warmup
+
 import config
 import dataset
+import engine
+from model import QuestionAnsweringModel
 
 def add_end_index(df, is_train=True):
     df_modified = df.copy()
@@ -139,8 +146,56 @@ if __name__ == "__main__":
     df_train = df_train.reset_index(drop=True)
     df_valid = df_valid.reset_index(drop=True)
 
-    train_dataset = dataset.QuestionAnsweringDataset(data=df_train)
+    train_dataset = dataset.QuestionAnsweringDataset(
+        data=df_train
+    )
 
     train_data_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=config.TRAIN_BATCH_SIZE, num_workers=4, shuffle=True
     )
+
+    valid_dataset = dataset.QuestionAnsweringDataset(
+        data=df_valid
+    )
+
+    valid_data_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=config.VALID_BATCH_SIZE, num_workers=1, shuffle=True
+    )
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    model = QuestionAnsweringModel()
+    model.to(device)
+
+    param_optimizer = list(model.named_parameters())
+    no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
+    optimizer_parameters = [
+        {
+            "params": [
+                p for n, p in param_optimizer if not any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.001,
+        },
+        {
+            "params": [
+                p for n, p in param_optimizer if any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.0,
+        },
+    ]
+
+    num_train_steps = int(len(df_train) / config.TRAIN_BATCH_SIZE * config.EPOCHS)
+    optimizer = AdamW(optimizer_parameters, lr=3e-5)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=0, num_training_steps=num_train_steps
+    )
+
+    best_f1 = 0
+    for epoch in range(config.EPOCHS):
+        train_em, train_f1 = engine.train_fn(train_data_loader, model, optimizer, device, scheduler)
+        test_em, test_f1 = engine.test_fn(valid_data_loader, model, device)
+        print(f"Train F1 = {train_f1} Valid F1 = {test_f1}")
+        if test_f1 > best_f1:
+            torch.save(model.state_dict(), config.MODEL_PATH)
+            best_f1 = test_f1
