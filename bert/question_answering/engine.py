363b9219d48afb3879e00508cf84724bb3194d82
ManashJKonwar <rickykonwar@gmail.com>
2022-10-04 23:19:49 +0530

    Enhancement: Adding_engine_script_for_question_answering_task
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- bert/question_answering/engine.py
+++ bert/question_answering/engine.py
@@ -6,3 +6,34 @@ __version__ = "1.0.1"
 __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
+
+from tqdm import tqdm
+
+def train_fn(data_loader, model, optimizer, device, scheduler):
+    model.train()
+    total_em_train = 0
+    total_f1_train = 0
+    for data in tqdm(data_loader, total=len(data_loader)):
+        input_ids = data['ids']
+        input_mask = data['mask']
+
+        input_ids = input_ids.to(device, dtype = torch.long)
+        input_mask = input_mask.to(device, dtype = torch.long)
+
+        outputs = model(input_ids, input_mask)
+    return total_em_train, total_f1_train
+
+def eval_fn(data_loader, model, device):
+    model.eval()
+    total_em_val = 0
+    total_f1_val = 0
+    with torch.no_grad():
+        for data in tqdm(data_loader, total=len(data_loader)):
+            input_ids = data['ids']
+            input_mask = data['mask']
+
+            input_ids = input_ids.to(device, dtype = torch.long)
+            input_mask = input_mask.to(device, dtype = torch.long)
+
+            outputs = model(input_ids, input_mask)
+        return total_em_val, total_f1_val
