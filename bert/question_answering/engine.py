c119a1f3fcd9bddc2ddbcfe6a43952fbbdb9f50a
ManashJKonwar <rickykonwar@gmail.com>
2022-10-07 23:00:35 +0530

    Enhancement: Setting_up_engine_script_for_question_answering_task
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- bert/question_answering/engine.py
+++ bert/question_answering/engine.py
@@ -7,33 +7,93 @@ __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
 
+import torch
+import numpy as np
+import torch.nn as nn
 from tqdm import tqdm
+
+from utils import get_batch_predictions, exact_match, f1_score
+
+def loss_fn(o1, o2, t1, t2):
+    l1 = nn.BCEWithLogitsLoss()(o1,t1)
+    l2 = nn.BCEWithLogitsLoss()(o2,t2)
+    return l1 + l2
 
 def train_fn(data_loader, model, optimizer, device, scheduler):
     model.train()
-    total_em_train = 0
-    total_f1_train = 0
+    average_em, average_f1 = 0.0, 0.0
+    running_loss = 0.0
     for data in tqdm(data_loader, total=len(data_loader)):
-        input_ids = data['ids']
-        input_mask = data['mask']
+        input_ids = data['input_ids']
+        mask = data['mask']
+        offset_mapping = data['offset_mapping']
+        token_type_ids = data['token_type_ids']
+        start_positions = data['start_positions']
+        end_positions = data['end_positions']
 
         input_ids = input_ids.to(device, dtype = torch.long)
-        input_mask = input_mask.to(device, dtype = torch.long)
+        mask = mask.to(device, dtype = torch.long)
+        offset_mapping = offset_mapping.to(device, dtype = torch.long)
+        token_type_ids = token_type_ids.to(device, dtype = torch.long)
+        start_positions = start_positions.to(device)
+        end_positions = end_positions.to(device)
 
-        outputs = model(input_ids, input_mask)
-    return total_em_train, total_f1_train
+        optimizer.zero_grad()
+        start_logits, end_logits = model(
+            input_ids=input_ids, 
+            mask=mask,
+            offset_mapping=offset_mapping,
+            token_type_ids=token_type_ids,
+        )
+
+        loss = loss_fn(start_logits, end_logits, start_positions, end_positions)
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        running_loss += loss.item()
+
+        # Calculating batch level em and f1 scores
+        predicted_answers = get_batch_predictions(data, start_logits, end_logits)
+        actual_answers = data['answers']
+        em = np.mean(exact_match(predicted_answers, actual_answers))
+        f1 = np.mean(f1_score(predicted_answers, actual_answers))
+        average_em += em
+        average_f1 += f1
+
+    average_em = average_em / (len(data_loader))
+    average_f1 = average_f1 / (len(data_loader))
+    training_loss = running_loss / len(data_loader)
+    return training_loss, average_em, average_f1
 
 def eval_fn(data_loader, model, device):
     model.eval()
-    total_em_val = 0
-    total_f1_val = 0
+    running_loss = 0.0
     with torch.no_grad():
         for data in tqdm(data_loader, total=len(data_loader)):
-            input_ids = data['ids']
-            input_mask = data['mask']
+            input_ids = data['input_ids']
+            mask = data['mask']
+            offset_mapping = data['offset_mapping']
+            token_type_ids = data['token_type_ids']
+            start_position = data['start_position']
+            end_position = data['end_position']
 
             input_ids = input_ids.to(device, dtype = torch.long)
-            input_mask = input_mask.to(device, dtype = torch.long)
+            mask = mask.to(device, dtype = torch.long)
+            offset_mapping = offset_mapping.to(device, dtype = torch.long)
+            token_type_ids = token_type_ids.to(device, dtype = torch.long)
+            start_position = start_position.to(device)
+            end_position = end_position.to(device)
 
-            outputs = model(input_ids, input_mask)
-        return total_em_val, total_f1_val
+            start_logits, end_logits = model(
+                input_ids=input_ids, 
+                mask=mask,
+                offset_mapping=offset_mapping,
+                token_type_ids=token_type_ids,
+            )
+
+            loss = loss_fn(start_logits, end_logits, start_position, end_position)
+            loss.backward()
+            running_loss += loss.item()
+        
+        valid_loss = running_loss/len(data_loader)
+        return valid_loss
