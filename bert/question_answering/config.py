35d9f4e72823a91d8028a048d8c0401c02676f37
ManashJKonwar <rickykonwar@gmail.com>
2022-10-10 10:42:05 +0530

    Update: Reducing_no_of_epochs
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- bert/question_answering/config.py
+++ bert/question_answering/config.py
@@ -12,7 +12,7 @@ import transformers
 MAX_LEN = 512
 TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 4
-EPOCHS = 10
+EPOCHS = 5
 BASE_MODEL_PATH = "pretrained_models/bert/bert_base_uncased"
 MODEL_PATH = "output/question_answering/model.bin"
 QnA_TRAINING_PATH = "input/question_answering/train-v1.1.json"
