a05ebe1a838dbbb088f44838bb59215741c29852
ManashJKonwar <rickykonwar@gmail.com>
2022-11-20 21:20:38 +0530

    Update: Adding_code_comments_for_engine_script_for_text_summarizer
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- bert/text_summarization/engine.py
+++ bert/text_summarization/engine.py
@@ -13,6 +13,23 @@ import torch.nn as nn
 from tqdm import tqdm
 
 def train_fn(data_loader, model, optimizer, device, scheduler):
+    """
+    This function is utilize to train the BERT architecture for the training sample and 
+    also calculate the loss for this epoch, store the relevant cross entropy loss and at the end return 
+    averaged out accuracy and loss for the train sample considering the predicted score for each sentence 
+    within the article followed by the actual top k scored sentences.
+
+    args: 
+    - data_loader: pytorch data loader object for the train sample
+    - model: BERT model which will be trained for predicted start and end logits for answers to relevant questions
+    - optimizer: Adam optimizer to adjust after each data pass
+    - device: pytorch device object to consider for training
+    - scheduler: schedule object with a learning rate that decreases linearly from the initial lr set in the optimizer to 0
+
+    return:
+    - total_acc_train / nb_tr_examples: averaged out accuracy for the entire train sample
+    - total_loss_train / len(data_loader): averaged out loss for the entire train sample
+    """
     model.train()
     total_acc_train = 0
     total_loss_train = 0
@@ -46,6 +63,20 @@ def train_fn(data_loader, model, optimizer, device, scheduler):
     return total_acc_train / nb_tr_examples, total_loss_train / len(data_loader) 
 
 def eval_fn(data_loader, model, device):
+    """
+    This function is utilize to evaluate the BERT architecture for the validation sample and 
+    also calculate the loss for this epoch, store the relevant loss and at the end return 
+    averaged out accuracy and loss for the validation sample
+
+    args: 
+    - data_loader: pytorch data loader object for the validation sample
+    - model: trained BERT model which will be validated for validating answer start and end logits
+    - device: pytorch device object to consider for validation
+
+    return:
+    - total_acc_val / nb_tr_examples: averaged out accuracy for the entire validation sample
+    - total_loss_val / len(data_loader): averaged out loss for the entire validation sample
+    """
     model.eval()
     total_acc_val = 0
     total_loss_val = 0
